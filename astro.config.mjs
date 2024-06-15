f167cdb3109a5af7a64ad7057040ebf980f68cde
Rylan McAlister <55506412+RylanMcA@users.noreply.github.com>
2024-06-15 03:41:35 -0500

    Added skeletons for various pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,14 +12,73 @@ export default defineConfig({
       github: 'https://github.com/lynn-lizard/lynn-lizard.github.io',
       discord: 'https://discord.gg/pebe'
     },
-    sidebar: [{
-      label: 'Guides',
+    sidebar: [	
+	{
+	label: 'Holsters',
+	items: [
+		// Each item here is one entry in the navigation menu.
+		{
+			label: 'Melee DPS',
+			link: '/holsters/melee'
+		},
+		{
+			label: 'Physical Ranged DPS',
+			link: '/holsters/phys'
+		},
+		{
+			label: 'Caster DPS',
+			link: '/holsters/caster'
+		},
+		{
+			label: 'Tanks',
+			link: '/holsters/tank'
+		},		
+		{
+			label: 'Healers',
+			link: '/holsters/healer'
+		}]
+	},	
+	{
+      label: 'Encounter Guides',
       items: [
       // Each item here is one entry in the navigation menu.
       {
-        label: 'Slimes Guide',
+        label: 'Slimes',
         link: '/guides/slimes'
-      }]
+      },
+	  {
+        label: 'Golems',
+        link: '/guides/golems'
+      },
+	  {
+        label: 'Trinity Seeker',
+        link: '/guides/seeker'
+      },
+	  {
+        label: 'Dahu',
+        link: '/guides/dahu'
+      },
+	  {
+        label: 'Queen\'s Guard',
+        link: '/guides/guards'
+      },
+	  {
+        label: 'Bozjan Phantom',
+        link: '/guides/phantom'
+      },
+	  {
+        label: 'Trinity Avowed',
+        link: '/guides/avowed'
+      },
+	  {
+        label: 'Stygimoloch Lord',
+        link: '/guides/lord'
+      },
+	  {
+        label: 'The Queen',
+        link: '/guides/queen'
+      },
+	]
     }, {
       label: 'Others',
       items: [
